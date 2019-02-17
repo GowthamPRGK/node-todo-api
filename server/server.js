26189c9e30ac9f3510c6aad9cdd21cf4dcf6c6cf
Gowtham Karnan <gowthamkarnan.prg@gmail.com>
2019-02-17 09:10:11 +0530

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -22,6 +22,15 @@ app.post('/todos',(req,res)=>{
     });
 });
 
+app.get('/todos',(req,res)=>{
+    Todo.find().then((todos)=>{
+        res.send({todos});
+    },(e)=>{
+        releaseEvents.status(400).send(e);
+    });
+
+});
+
 
 
 app.listen(3000,()=>{
